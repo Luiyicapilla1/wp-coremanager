421d4d4a15d6b50069b118a41d5a025f09459f4b
Luis Garcia <luiyicapilla@gmail.com>
2026-04-30 21:38:40 +0200

    feat(Queries): implementación de queries para sincronizacion de ram, cpu, placa base, y gpu

--- queries/queries.php
+++ queries/queries.php
@@ -3,25 +3,217 @@
 //---------------------------------------
 // Constant var almacenamiento query
 //---------------------------------------
-define('ALMACENAMIENTO_QUERY', '');
+define('ALMACENAMIENTO_QUERY', '
+    query {
+        getProductoListing(defaultLanguage: "es", filter: "{\"Tipo\": \"disco duro\"}") {
+            edges {
+                node {
+                    id
+                    modificationDate
+                    sku_es: sku(language: "es")
+                    sku_en: sku(language: "en")
+                    sku_fr: sku(language: "fr")
+                    ean_es: ean(language: "es")
+                    ean_en: ean(language: "en")
+                    ean_fr: ean(language: "fr")
+                    nombre_es: nombre(language: "es")
+                    nombre_en: nombre(language: "en")
+                    nombre_fr: nombre(language: "fr")
+                    slug_es: slug(language: "es")
+                    slug_en: slug(language: "en")
+                    slug_fr: slug(language: "fr")
+                    iva
+                    stock
+                    imagen {
+                        fullpath
+                    }
+                    galeria {
+                        image {
+                            fullpath
+                        }
+                    }
+                    descripcion_es: descripcion(language: "es")
+                    descripcion_en: descripcion(language: "en")
+                    descripcion_fr: descripcion(language: "fr")
+                    titulo_seo_es: titulo_seo(language: "es")
+                    titulo_seo_en: titulo_seo(language: "en")
+                    titulo_seo_fr: titulo_seo(language: "fr")
+                    descripcion_seo_es: descripcion_seo(language: "es")
+                    descripcion_seo_en: descripcion_seo(language: "en")
+                    descripcion_seo_fr: descripcion_seo(language: "fr")
+                    tipo_almacenamiento
+                    interface
+                    capacity
+                    writespeed
+                    readspeed
+                }
+            }
+        }
+    }
+');
 
 
 //---------------------------------------
 // Constant var ram query
 //---------------------------------------
-define('RAM_QUERY', '');
+define('RAM_QUERY', '
+    query {
+        getProductoListing(defaultLanguage: "es", filter: "{\"Tipo\": \"memoria\"}") {
+            edges {
+                node {
+                    id
+                    modificationDate
+                    sku_es: sku(language: "es")
+                    sku_en: sku(language: "en")
+                    sku_fr: sku(language: "fr")
+                    ean_es: ean(language: "es")
+                    ean_en: ean(language: "en")
+                    ean_fr: ean(language: "fr")
+                    nombre_es: nombre(language: "es")
+                    nombre_en: nombre(language: "en")
+                    nombre_fr: nombre(language: "fr")
+                    slug_es: slug(language: "es")
+                    slug_en: slug(language: "en")
+                    slug_fr: slug(language: "fr")
+                    iva
+                    stock
+                    imagen {
+                        fullpath
+                    }
+                    galeria {
+                        image {
+                            fullpath
+                        }
+                    }
+                    descripcion_es: descripcion(language: "es")
+                    descripcion_en: descripcion(language: "en")
+                    descripcion_fr: descripcion(language: "fr")
+                    titulo_seo_es: titulo_seo(language: "es")
+                    titulo_seo_en: titulo_seo(language: "en")
+                    titulo_seo_fr: titulo_seo(language: "fr")
+                    descripcion_seo_es: descripcion_seo(language: "es")
+                    descripcion_seo_en: descripcion_seo(language: "en")
+                    descripcion_seo_fr: descripcion_seo(language: "fr")
+                    ramtype
+                    capacity
+                    modules
+                    speed
+                    latency
+                    formfactor
+                }
+            }
+        }
+    }
+');
 
 
 //---------------------------------------
 // Constant var cpu query
 //---------------------------------------
-define('CPU_QUERY', '');
+define('CPU_QUERY', '
+    query {
+        getProductoListing(defaultLanguage: "es", filter: "{\"Tipo\": \"cpu\"}") {
+            edges {
+                node {
+                    id
+                    modificationDate
+                    sku_es: sku(language: "es")
+                    sku_en: sku(language: "en")
+                    sku_fr: sku(language: "fr")
+                    ean_es: ean(language: "es")
+                    ean_en: ean(language: "en")
+                    ean_fr: ean(language: "fr")
+                    nombre_es: nombre(language: "es")
+                    nombre_en: nombre(language: "en")
+                    nombre_fr: nombre(language: "fr")
+                    slug_es: slug(language: "es")
+                    slug_en: slug(language: "en")
+                    slug_fr: slug(language: "fr")
+                    iva
+                    stock
+                    imagen {
+                        fullpath
+                    }
+                    galeria {
+                        image {
+                            fullpath
+                        }
+                    }
+                    descripcion_es: descripcion(language: "es")
+                    descripcion_en: descripcion(language: "en")
+                    descripcion_fr: descripcion(language: "fr")
+                    titulo_seo_es: titulo_seo(language: "es")
+                    titulo_seo_en: titulo_seo(language: "en")
+                    titulo_seo_fr: titulo_seo(language: "fr")
+                    descripcion_seo_es: descripcion_seo(language: "es")
+                    descripcion_seo_en: descripcion_seo(language: "en")
+                    descripcion_seo_fr: descripcion_seo(language: "fr")
+                    socket
+                    cores
+                    threads
+                    baseclock
+                    boostclock
+                    tdp
+                    architecture
+                }
+            }
+        }
+    }
+');
 
 
 //---------------------------------------
 // Constant var placa base query
 //---------------------------------------
-define('PLACA_BASE_QUERY', '');
+define('PLACA_BASE_QUERY', '
+    query {
+        getProductoListing(defaultLanguage: "es", filter: "{\"Tipo\": \"placa base\"}") {
+            edges {
+                node {
+                    id
+                    modificationDate
+                    sku_es: sku(language: "es")
+                    sku_en: sku(language: "en")
+                    sku_fr: sku(language: "fr")
+                    ean_es: ean(language: "es")
+                    ean_en: ean(language: "en")
+                    ean_fr: ean(language: "fr")
+                    nombre_es: nombre(language: "es")
+                    nombre_en: nombre(language: "en")
+                    nombre_fr: nombre(language: "fr")
+                    slug_es: slug(language: "es")
+                    slug_en: slug(language: "en")
+                    slug_fr: slug(language: "fr")
+                    iva
+                    stock
+                    imagen {
+                        fullpath
+                    }
+                    galeria {
+                        image {
+                            fullpath
+                        }
+                    }
+                    descripcion_es: descripcion(language: "es")
+                    descripcion_en: descripcion(language: "en")
+                    descripcion_fr: descripcion(language: "fr")
+                    titulo_seo_es: titulo_seo(language: "es")
+                    titulo_seo_en: titulo_seo(language: "en")
+                    titulo_seo_fr: titulo_seo(language: "fr")
+                    descripcion_seo_es: descripcion_seo(language: "es")
+                    descripcion_seo_en: descripcion_seo(language: "en")
+                    descripcion_seo_fr: descripcion_seo(language: "fr")
+                    socket
+                    chipset
+                    form_factor
+                    ramtype
+                    ramslots
+                    m2slots
+                }
+            }
+        }
+    }
+');
 
 
 //---------------------------------------
