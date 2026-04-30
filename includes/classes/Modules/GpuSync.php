2ca67fb5964ad5cff6b752850cf3a20a26f39b5a
Luis <luisgarciacapilla1@gmail.com>
2026-04-30 21:55:18 +0200

    feat(GpuSync): actualizar nombre de campos de origen de datos para sincronizacion

--- includes/classes/Modules/GpuSync.php
+++ includes/classes/Modules/GpuSync.php
@@ -98,19 +98,19 @@ class GpuSync extends Module
                     //---------------------------------------
                     // ACF Fields Relations
                     //---------------------------------------
-                    update_field('sku', $post['url_' . $language], $post_id);
-                    update_field('ean', $post['url_' . $language], $post_id);
-                    update_field('nombre', $post['url_' . $language], $post_id);
-                    update_field('slug', $post['url_' . $language], $post_id);
-                    update_field('ivan', $post['url_' . $language], $post_id);
-                    update_field('stock', $post['url_' . $language], $post_id);
-                    update_field('descripcion', $post['url_' . $language], $post_id);
-                    update_field('titulo_seo', $post['url_' . $language], $post_id);
-                    update_field('descripcion_seo', $post['url_' . $language], $post_id);
-                    update_field('chipset', $post['url_' . $language], $post_id);
-                    update_field('vram', $post['url_' . $language], $post_id);
-                    update_field('vramtype', $post['url_' . $language], $post_id);
-                    update_field('tdp', $post['url_' . $language], $post_id);
+                    update_field('sku', $post['sku_' . $language], $post_id);
+                    update_field('ean', $post['ean_' . $language], $post_id);
+                    update_field('nombre', $post['nombre_' . $language], $post_id);
+                    update_field('slug', $post['slug_' . $language], $post_id);
+                    update_field('ivan', $post['iva'], $post_id);
+                    update_field('stock', $post['stock'], $post_id);
+                    update_field('descripcion', $post['descripcion_' . $language], $post_id);
+                    update_field('titulo_seo', $post['titulo_seo_' . $language], $post_id);
+                    update_field('descripcion_seo', $post['descripcion_seo_' . $language], $post_id);
+                    update_field('chipset', $post['chipset'], $post_id);
+                    update_field('vram', $post['vram'], $post_id);
+                    update_field('vramtype', $post['vramtype'], $post_id);
+                    update_field('tdp', $post['tdp'], $post_id);
                 }
 
                 //Modification Date Post Meta
