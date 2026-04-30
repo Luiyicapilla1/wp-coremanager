205ab88344ef9a4ccb251eae28805c75bb54aa12
Luis <luisgarciacapilla1@gmail.com>
2026-04-30 21:03:32 +0200

    feat(PlacaBaseSync): Implementar sincronización de datos para "PlacaBase"

---
+++ includes/classes/Modules/PlacaBaseSync.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace wp_coremanager\Modules;
+
+use wp_coremanager\Module;
+use wp_coremanager\Services\GraphQLClient;
+
+set_time_limit(0);
+
+class PlacaBaseSync extends Module
+{
+    public $load_order = 15;
+    private const PIM_URL = 'http://127.0.0.1:8080';
+
+    public function puede_registrar()
+    {
+        return true;
+    }
+
+    public function registrar()
+    {
+        // Noncompliant - method is empty
+    }
+
+    public function sync_placa_base(bool $force = false)
+    {
+        require_once CM_PLUGIN_PATH . 'queries/queries.php';
+        $client = new GraphQLClient;
+        $query = \PLACA_BASE_QUERY;
+        $data = $client->query($query);
+        foreach ($data['getProductoListing']['edges'] as $edge) {
+            $post = $edge['node'];
+            $languages = ['es', 'en', 'fr'];
+            $post_languages_list = [
+                'es' => null,
+                'en' => null,
+                'fr' => null
+            ];
+            foreach ($languages as $language) {
+                $exist = get_posts([
+                    'post_type' => 'placa-base',
+                    'lang' => $language,
+                    'meta_key' => 'pimcore_id',
+                    'meta_value' => $post['id'],
+                    'numberposts' => 1,
+                ]);
+
+                if (!empty($exist)) {
+                    $post_id = $exist[0]->ID;
+                    wp_update_post([
+                        'ID' => $post_id,
+                        'lang' => $language,
+                        'post_title' => $post['nombre_' . $language],
+                        'post_content' => $post['descripcion_' . $language],
+                    ]);
+                } else {
+                    $post_id = wp_insert_post([
+                        'post_type' => 'placa-base',
+                        'lang' => $language,
+                        'post_title' => $post['nombre_' . $language],
+                        'post_content' => $post['descripcion_' . $language],
+                        'post_status' => 'publish',
+                        'post_autor' => 1,
+                    ]);
+                    pll_set_post_language($post_id, $language);
+
+                    update_post_meta($post_id, 'pimcore_id', $post['id']);
+                }
+                $post_idioms_list = $this->post_languages_relations($post_id, $post_languages_list);
+
+                if ($post['modificationDate'] != get_post_meta($post_id, 'modification_date')[0] || $force) {
+
+
+                    //---------------------------------------
+                    // Logic Images
+                    //---------------------------------------
+                    $image_id = get_field('imagen', $post_id);
+                    if ($image_id != 121471) {
+                        wp_delete_attachment($image_id, true);
+                    } else {
+                        update_field('imagen', null, $post_id);
+                    }
+                    if ($language == 'es') {
+                        $image_post = $this->set_image_from_wp_media(self::PIM_URL . $post['imagen']['fullpath'], $post_id);
+                        update_field('imagen', $image_post, $post_id);
+                        FolderModel::setFoldersForPosts($image_post, $folder);
+                    } else {
+                        if ($image_post != null) {
+                            update_field('imagen', $image_post, $post_id);
+                            FolderModel::setFoldersForPosts($image_post, $folder);
+                        }
+                    }
+                    //PLACEHOLDER
+                    if (get_field('imagen', $post_id) == null) {
+                        update_field('imagen', 121471, $post_id);
+                    }
+
+                    //---------------------------------------
+                    // ACF Fields Relations
+                    //---------------------------------------
+                    update_field('sku', $post['url_' . $language], $post_id);
+                    update_field('ean', $post['url_' . $language], $post_id);
+                    update_field('nombre', $post['url_' . $language], $post_id);
+                    update_field('slug', $post['url_' . $language], $post_id);
+                    update_field('ivan', $post['url_' . $language], $post_id);
+                    update_field('stock', $post['url_' . $language], $post_id);
+                    update_field('descripcion', $post['url_' . $language], $post_id);
+                    update_field('titulo_seo', $post['url_' . $language], $post_id);
+                    update_field('descripcion_seo', $post['url_' . $language], $post_id);
+                    update_field('socket', $post['url_' . $language], $post_id);
+                    update_field('chipset', $post['url_' . $language], $post_id);
+                    update_field('form_factor', $post['url_' . $language], $post_id);
+                    update_field('ramtype', $post['url_' . $language], $post_id);
+                    update_field('ramslots', $post['url_' . $language], $post_id);
+                    update_field('m2slots', $post['url_' . $language], $post_id);
+                }
+
+                //Modification Date Post Meta
+                update_post_meta($post_id, 'modification_date', $post['modificationDate']);
+            }
+            pll_save_post_translations($post_idioms_list);
+        }
+    }
+}
